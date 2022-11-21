eb362f7b8a4770a9ff5b4003eb60ce74222db4c8
morty <you@example.com>
2022-11-21 12:09:06 +0800

    模板template

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"github.com/myusername/gee-demo/gee"
+	"html/template"
 	"log"
 	"net/http"
 	"time"
@@ -19,22 +21,49 @@ func onlyForV2() gee.HandlerFunc {
 	}
 }
 
+type student struct {
+	Name string
+	Age  int8
+}
+
+func FormatAsDate(t time.Time) string {
+	year, month, day := t.Date()
+	return fmt.Sprintf("%d-%02d-%02d", year, month, day)
+}
+
 func main() {
 	r := gee.New()
 	r.Use(gee.Logger()) // global midlleware
+	r.SetFuncMap(template.FuncMap{
+		"FormatAsDate": FormatAsDate,
+	})
+	r.LoadHTMLGlob("templates/*")
+	r.Static("/assets", "./static")
+	stu1 := &student{Name: "Geektutu", Age: 20}
+	stu2 := &student{Name: "Jack", Age: 22}
+
 	r.GET("/", func(c *gee.Context) {
-		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+		c.HTML(http.StatusOK, "css.tmpl", nil)
 	})
 
 	v1 := r.Group("/v1")
 	{
-		v1.GET("/", func(c *gee.Context) {
-			c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
-		})
-
 		v1.GET("/hello", func(c *gee.Context) {
 			// expect /hello?name=geektutu
 			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+		})
+		v1.GET("/students", func(c *gee.Context) {
+			c.HTML(http.StatusOK, "arr.tmpl", gee.H{
+				"title":  "gee",
+				"stuArr": [2]*student{stu1, stu2},
+			})
+		})
+
+		v1.GET("/date", func(c *gee.Context) {
+			c.HTML(http.StatusOK, "custom_func.tmpl", gee.H{
+				"title": "gee",
+				"now":   time.Date(2019, 8, 17, 0, 0, 0, 0, time.UTC),
+			})
 		})
 	}
 	v2 := r.Group("/v2")
